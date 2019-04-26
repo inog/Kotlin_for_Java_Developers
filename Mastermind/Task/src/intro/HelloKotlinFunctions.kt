21c6fd22d75d1e3177438fcfba1a98db1d4e6dad
Ingo <kontakt@ingo-reschke.de>
2019-04-26 21:45:42 +0200

    diff between arguments and named arguments

--- Mastermind/Task/src/intro/HelloKotlinFunctions.kt
+++ Mastermind/Task/src/intro/HelloKotlinFunctions.kt
@@ -4,6 +4,9 @@ fun main(args:Array<String>){
     println( max(5,3))
     println( maxx(345,31232))
     join()
+    displaySeperator('3',5)
+    println()
+    displaySeperator(size=3, character = '5') //named arguments
 }
 
 // block body
@@ -17,5 +20,11 @@ fun maxx(a:Int , b:Int) = if (a > b) a else b
 // named arguments
 fun join(){
     println(listOf('a','b','c').joinToString ( "","[","]" ))
+}
+
+fun displaySeperator(character: Char = '*', size: Int = 10){
+    repeat(size){
+        print(character)
+    }
 }
 
